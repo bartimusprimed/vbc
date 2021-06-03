7fec42fd4fd2dfcb0a6d79773dfcea7a07f78ab2
bartimus <null>
2021-06-03 11:45:07 -0400

    Added some more docs, as well as removing some "&" references.

--- chain/transaction.v
+++ chain/transaction.v
@@ -1,6 +1,10 @@
 module chain 
 import entities
 
+// Transaction is a very simple structure, it consists of `to` and `from` of type `&entities.User`
+// the transaction contains an `item` of type `string`. I originally had this item as a generic
+// but I was running into issues with heap structures being generic, when attempting to hash them
+// might revisit this when generics are more seasoned.
 [heap]
 pub struct Transaction {
 	to &entities.User
@@ -8,24 +12,33 @@ pub struct Transaction {
 	item string
 }
 
+
+// Transaction_Chain holds a list of transactions that are not applied to the `Block_Chain`.
+// When commited the blockchain the transaction chain gets wiped.
 [heap]
 pub struct Transaction_Chain {
 pub mut:
 	transactions []&Transaction
 }
 
+// flushed_transaction_chain resets the list to empty. Should only be called after a successful commit.
 pub fn (mut tc Transaction_Chain) flush_transaction_chain() {
 	tc.transactions = []&Transaction{}
 }
 
+// new_transaction needs a `to` and `from` `&entities.User` as well as an item that is being tracked.
+// and returns a transaction structure that can be applied to the transaction chain.
 pub fn new_transaction(to &entities.User, from &entities.User, item string) &Transaction {
 	return &Transaction{to: to, from: from, item: item}
 }
 
+// new_transaction_chain creates a new list of transactions, I was going to join this into the blockchain
+// structure, but wanted to keep the option open of having multiple transaction chains that can be queued
 pub fn new_transaction_chain() &Transaction_Chain {
 	return &Transaction_Chain {}
 }
 
+// to_string converts the `Transaction_Chain` to a string, used for hashing.
 pub fn (tc Transaction_Chain) to_string() string {
 	return "$tc"
 }
--- entities/users.v
+++ entities/users.v
@@ -1,13 +1,19 @@
 module entities
+// The entities module will eventually hold things not related to the chain itself
+// right now only users exists, but I will probably add machine structures to track
+// if a user has multiple machines.
 
+// User is very simple right now, probably needs a hash and other stuff so the blockchain
+// can keep them private.
 [heap]
 pub struct User {
 	name string
 	identity int
 }
 
-pub fn new_user(name string, identity int) &User {
-	return &User{
+// new_user is a simple function that returns a User.
+pub fn new_user(name string, identity int) User {
+	return User{
 		name: name
 		identity: identity
 	}
